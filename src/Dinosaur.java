cab93cbda03c20b35a0ea4dfead78bc8aa9b991f
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2024-11-26 10:43:14 +0100

    skapade slumptals generatorer samt max- och minvärden för de nämnda önskemålen kring skillnad i slumputtslag, jag genomför dessa genereringar i metoder som återger resultatvärde.

--- src/Dinosaur.java
+++ src/Dinosaur.java
@@ -1,3 +1,19 @@
-public class Dinosaur extends Creature{
+import java.util.Random;
 
+public class Dinosaur extends Creature{
+    Random generator = new Random();
+
+    public int minIntelligenceDino = 10;
+    public int maxIntelligenceDino = 15;
+    public int minEnduranceDino = 5;
+    public int maxEnduranceDino = 10;
+
+    public int dinoIntelligence(){
+        int result = generator.nextInt(minIntelligenceDino, maxIntelligenceDino);
+        return result;
+    }
+    public int dinoEndurance(){
+        int result = generator.nextInt(minEnduranceDino, maxEnduranceDino);
+        return result;
+    }
 }
--- src/Zombie.java
+++ src/Zombie.java
@@ -1,3 +1,19 @@
-public class Zombie extends Creature{
+import java.util.Random;
 
+public class Zombie extends Creature{
+    Random generator = new Random();
+
+    public int minIntelligenceZombie = 5;
+    public int maxIntelligenceZombie = 10;
+    public int minEnduranceZombie = 10;
+    public int maxEnduranceZombie = 15;
+
+    public int zombieIntelligence(){
+        int result = generator.nextInt(minIntelligenceZombie, maxIntelligenceZombie);
+        return result;
+    }
+    public int zombieEndurance(){
+        int result = generator.nextInt(minEnduranceZombie, maxEnduranceZombie);
+        return result;
+    }
 }
