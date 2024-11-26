a4a7421ba5d2818d009e2f8b031d12c976ff2bcc
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2024-11-26 11:20:15 +0100

    lade till randomgenerator samt maxvärde för de tre metoderna där lagen möts.

--- src/Creature.java
+++ src/Creature.java
@@ -1,9 +1,14 @@
+import java.util.Random;
+
 public class Creature {
     // Variabler
     public String name;
     public int intelligence;
     public int strength;
     public int endurance;
+
+    public Random genRandom = new Random();
+    public int maxValue = 30;
 
     // Metoder
     public void playChess(){
