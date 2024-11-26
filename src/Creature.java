9414b566e188c19936fc1d1769492ed09222c25f
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2024-11-26 10:53:31 +0100

    lade till en arraylista för att lagra varelser (creatures) samt metoder för att visa listans innehåll samt lägga till varelser i listan.

--- src/Creature.java
+++ src/Creature.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList;
+
 public class Creature {
     // Variabler
+    public int name;
     public int intelligence;
     public int strength;
     public int endurance;
+    public ArrayList<Creature> Creatures = new ArrayList<>();
 
     // Metoder
+    public void displayCreatures(){
+        for (Creature creature : Creatures){
+            System.out.println("- "+creature.name);
+            System.out.println("  - "+creature.intelligence);
+            System.out.println("  - "+creature.strength);
+            System.out.println("  - "+creature.endurance);
+        }
+    }
+    public void add(Creature creature){
+        Creatures.add(creature);
+    }
     public void playChess(){
         //jj (return int later)
     }
